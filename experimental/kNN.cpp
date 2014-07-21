453953ab29b0f11776876bedeb688237076eee94
Cris Cecka <ccecka@seas.harvard.edu>
2014-07-21 14:42:46 -0400

    Outline of single-tree kNN

--- experimental/kNN.cpp
+++ experimental/kNN.cpp
@@ -2,10 +2,15 @@
 #include <vector>
 #include <functional>
 #include <algorithm>
+#include <stack>
 
 #include "fmmtl/Kernel.hpp" //Ugh
 #include "fmmtl/Direct.hpp"
 #include "fmmtl/numeric/random.hpp"
+#include "fmmtl/tree/NDTree.hpp"
+#include "fmmtl/tree/TreeData.hpp"
+#include "fmmtl/tree/TreeRange.hpp"
+
 
 template <typename T, std::size_t K, typename Compare = std::less<T> >
 class ordered_vector {
@@ -44,6 +49,13 @@ class ordered_vector {
     return std::vector<T>(comp.data, comp.data + K);
   }
 
+  bool operator==(const ordered_vector& v) {
+    return std::equal(comp.data, comp.data+K, v.comp.data);
+  }
+  bool operator!=(const ordered_vector& v) {
+    return !(*this == v);
+  }
+
   friend std::ostream& operator<<(std::ostream& s, const ordered_vector& ov) {
     s << "(";
     if (ov.size >= 1)
@@ -55,27 +67,46 @@ class ordered_vector {
 };
 
 
+/** kNN Kernel implementing
+ * r_i += K(t_i, s_j) c_j
+ * where K(t_i, s_j) = ||t_i - s_j||^2
+ * r_i is a sorted vector of the K smallest values seen
+ * and c_j = 1 (or perhaps the original index and multiplication becomes append)
+ */
 template <std::size_t K>
 struct kNN : public fmmtl::Kernel<kNN<K> >{
-  typedef int source_type;
-  typedef int target_type;
-  typedef int charge_type;
-  typedef ordered_vector<int,K> result_type;
+  typedef Vec<1,double> source_type;
+  typedef Vec<1,double> target_type;
+  typedef int    charge_type;
 
-  typedef int kernel_value_type;
+  typedef double kernel_value_type;
+  typedef ordered_vector<double,K> result_type;
 
   kernel_value_type operator()(const target_type& t,
                                const source_type& s) const {
-    (void) t;
-    return s;
-    //return std::abs(s-t);
+    return norm_2_sq(t-s);
   }
 };
 
 
 
 
-int main() {
+int main(int argc, char** argv) {
+  int N = 1000;
+  int M = 1000;
+  bool checkErrors = true;
+
+  // Parse custom command line args
+  for (int i = 1; i < argc; ++i) {
+    if (strcmp(argv[i],"-N") == 0) {
+      N = atoi(argv[++i]);
+    } else if (strcmp(argv[i],"-M") == 0) {
+      M = atoi(argv[++i]);
+    } else if (strcmp(argv[i],"-nocheck") == 0) {
+      checkErrors = false;
+    }
+  }
+
   typedef kNN<5> Kernel;
   typedef typename Kernel::source_type source_type;
   typedef typename Kernel::charge_type charge_type;
@@ -84,17 +115,11 @@ int main() {
 
   Kernel K;
 
-  std::cout << sizeof(result_type) << std::endl;
-
-  int N = 1000;
-  int M = 1000;
-
   std::vector<source_type> sources(M);
   for (auto&& s : sources)
     s = fmmtl::random<source_type>::get();
-  sources[3] = 3;
-  sources[1] = 1;
 
+  // TODO: Implicit values
   std::vector<charge_type> charges(M);
   for (auto&& c : charges)
     c = 1;
@@ -105,10 +130,90 @@ int main() {
 
   std::vector<result_type> results(N);
 
-  Direct::matvec(K, sources, charges, targets, results);
+  // Construct the source tree
+  constexpr unsigned D = fmmtl::dimension<source_type>::value;
+  fmmtl::NDTree<D> source_tree(sources);
 
-  for (auto&& r : results)
-    std::cout << r << std::endl;
+  typedef fmmtl::NDTree<D>::box_type  source_box_type;
+  typedef fmmtl::NDTree<D>::body_type source_body_type;
+
+  // Hyperrectangular distance struct
+  struct hyper_rect_distance {
+    double hyperrect_distance;
+    double farthest_distance;   // XXX: result[k].back()?
+    Vec<D,double> axis;
+  };
+
+  // Associate each box of the source tree with a hyperrectangular distance
+  auto h_rect = make_box_binding<hyper_rect_distance>(source_tree);
+  auto p_sources = make_body_binding(sources, source_tree);
+  auto p_charges = make_body_binding(charges, source_tree);
+
+  //
+  // Traversal -- Single-Tree
+  //
+
+  // For each target
+  for (unsigned k = 0; k < targets.size(); ++k) {
+
+    // Start from the root of the source tree
+    std::stack<source_box_type> stack;
+    stack.push(source_tree.root());
+
+    while (!stack.empty()) {
+      source_box_type sbox = stack.top();
+      stack.pop();
+
+      // If leaf, compute direct evaluation
+      if (sbox.is_leaf()) {
+        // TODO: Cleanup with fmmtl::direct rewrite (range/scalar comprehension)
+        auto source_range = p_sources[sbox];
+        auto charge_range = p_charges[sbox];
+        Direct::matvec(K,
+                       source_range.first, source_range.second,  // Source range
+                       charge_range.first,                       // Source charge
+                       targets.begin()+k, targets.begin()+k+1,   // Target range
+                       results.begin()+k);                       // Target result
+      } else {
+        // Update intersection data h_rect[sbox]
+        auto& box_data = h_rect[sbox];
+        source_box_type pbox = sbox.parent();
+        // box_data.hyperrect_distance = ...
+
+        // Attempt to prune this box based on the computed data
+
+        // Sort the children in the order we would like them traversed
+
+        // Add children (in reserve order) to the stack for traversal
+      }
+    }
+  }
+
+
+  //
+  // Complete
+  //
+
+  // Check the result
+  if (checkErrors) {
+    std::cout << "Computing direct matvec..." << std::endl;
+
+    std::vector<result_type> exact(M);
+
+    // Compute the result with a direct matrix-vector multiplication
+    Direct::matvec(K, sources, charges, targets, exact);
+
+    int wrong_results = 0;
+    for (unsigned k = 0; k < results.size(); ++k) {
+      if (exact[k] != results[k]) {
+        std::cout << "[" << std::setw(log10(M)+1) << k << "]"
+                  << " Exact: " << exact[k]
+                  << ", Tree: " << results[k] << std::endl;
+        ++wrong_results;
+      }
+    }
+    std::cout << "Wrong counts: " << wrong_results << " of " << M << std::endl;
+  }
 
   return 0;
 }
---
+++ fmmtl/tree/TreeData.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+
+/** Maps boxes and box iterators to data and data iterators
+ */
+template <typename T, typename Tree>
+struct BoxBind {
+  typedef std::vector<T> container_type;
+  typedef typename container_type::iterator iterator;
+  typedef typename container_type::const_iterator const_iterator;
+
+  container_type data;
+
+  BoxBind(const Tree&, unsigned size)
+      : data(size) {
+  }
+
+  T& operator[](const typename Tree::box_type& box) {
+    return data[box.index()];
+  }
+  const T& operator[](const typename Tree::box_type& box) const {
+    return data[box.index()];
+  }
+
+  iterator operator[](const typename Tree::box_iterator& bi) {
+    return data.begin() + (*bi).index();
+  }
+  const_iterator operator[](const typename Tree::box_iterator& bi) const {
+    return data.begin() + (*bi).index();
+  }
+};
+
+template <typename T, typename Tree>
+BoxBind<T,Tree> make_box_binding(const Tree& tree) {
+  return {tree, tree.boxes()};
+}
+
+
+/** Maps bodies and body iterators to data and data iterators
+ */
+template <typename T, typename Tree>
+struct BodyBind {
+  typedef std::vector<T> container_type;
+  typedef typename container_type::iterator iterator;
+  typedef typename container_type::const_iterator const_iterator;
+
+  //const Tree& tree;
+  container_type data;
+
+  BodyBind(const Tree& tree)
+      : data(tree.bodies()) {
+  }
+  template <typename Range>
+  BodyBind(const Tree& tree, const Range& range)
+      : data(tree.body_permute(range.begin(), tree.body_begin()),
+             tree.body_permute(range.begin(), tree.body_end())) {
+  }
+
+  T& operator[](const typename Tree::body_type& body) {
+    return data[body.index()];
+  }
+  const T& operator[](const typename Tree::body_type& body) const {
+    return data[body.index()];
+  }
+
+  iterator operator[](const typename Tree::body_iterator& bi) {
+    return data.begin() + (*bi).index();
+  }
+  const_iterator operator[](const typename Tree::body_iterator& bi) const {
+    return data.begin() + (*bi).index();
+  }
+
+  std::pair<iterator,iterator> operator[](const typename Tree::box_type& box) {
+    return {(*this)[box.body_begin()], (*this)[box.body_end()]};
+  }
+};
+
+template <typename Range, typename Tree>
+BodyBind<typename Range::value_type,Tree>  // XXX Range::value_type
+make_body_binding(const Range& c, const Tree& tree) {
+  return {tree, c};
+}
